2cd4e99126e973892e497a8d6b52295d8f429597
laj <larry.jones@reveal-energy.com>
2022-09-24 13:30:45 -0500

    Use Array.length instead of Length property

--- l02/MyFirstFSharpApp/MyFirstFSharpApp/Program.fs
+++ l02/MyFirstFSharpApp/MyFirstFSharpApp/Program.fs
@@ -1,7 +1,7 @@
 ﻿[<EntryPoint>]  // The `EntryPoint` attribute identifies the entry point of the entire application
 let main args =
     // Print the command line arguments
-    printfn $"Console application passed %d{args.Length} command line arguments %A{args}"
+    printfn $"Console application passed %d{Array.length args} command line arguments %A{args}"
     
     // A return value of 0 becomes a success exit code for the process.
     0
