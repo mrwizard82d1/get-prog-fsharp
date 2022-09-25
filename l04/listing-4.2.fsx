7ceaf4ee8846302536e0c57152e2e40197047dd4
laj <larry.jones@reveal-energy.com>
2022-09-25 13:11:09 -0500

    Shadowing variable is FSI

---
+++ l04/listing-4.2.fsx
@@ -0,0 +1,15 @@
+﻿// Shadowing in FSI
+
+let foo() =
+    let x = 10  // Bind `x` for the first time
+    printfn $"First: %d{x + 20}"
+    
+    let x = "test"  // Hide (shadow) previous binding. Only in FSI.
+    printfn $"Second: %s{x}"
+    
+    let x = 50.0  // Hide (shadow) previous binding. Only in FSI.
+    printfn $"Third: %f{x + 200.0}"
+    x + 200.0
+    
+foo()
+    
