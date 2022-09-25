01326825d27dec8a7115af6c0f46aa3fea3bafb3
laj <larry.jones@reveal-energy.com>
2022-09-24 19:25:05 -0500

    Add scratchpad script

--- l02/MyFirstFSharpApp/MyFirstFSharpApp/scratchpad.fsx
+++ l02/MyFirstFSharpApp/MyFirstFSharpApp/scratchpad.fsx
@@ -5,3 +5,14 @@ let greetPerson name age =
     $"Hello, %s{name}. You are %d{age} years old."
 let greeting = greetPerson "Fred" 25
 
+let countWords (text:string) =
+    Array.length (text.Split ())
+let toSplit = "Four score and seven years ago, our fathers brought forth on this continent" +
+              " a new nation, conceived in liberty and dedicated to the proposition" +
+              " that all men are equal."
+let wordCount = countWords toSplit
+
+toSplit
+wordCount
+
+System.IO.File.WriteAllText("./word-count.txt", sprintf $"The string, '%s{toSplit}', has %d{wordCount} words.")
