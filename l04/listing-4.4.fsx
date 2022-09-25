4cf32447e9ec82d082bf466825e8eac1eb39c916
laj <larry.jones@reveal-energy.com>
2022-09-25 13:19:54 -0500

    Scoping in F#

---
+++ l04/listing-4.4.fsx
@@ -0,0 +1,11 @@
+﻿// Scoping in F#
+
+// No accessibility modifier (all top-level values are `public` by default).
+// No static modifier (C# requires all functions (lambdas?) to be defined in a class scope).
+let doStuffWithTwoNumbers first second =
+    let added = first + second
+    printfn $"%d{first} + %d{second} = %d{added}"
+    let doubled = added * 2
+    doubled  // No `return` keyword needed; return last expression of scope
+    
+doStuffWithTwoNumbers 3 5
