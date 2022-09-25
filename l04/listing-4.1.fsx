71c516da91af11a02cb78fce23de2f4afaa94432
laj <larry.jones@reveal-energy.com>
2022-09-24 21:33:30 -0500

    Create project. Script simple types.

---
+++ l04/listing-4.1.fsx
@@ -0,0 +1,8 @@
+﻿// A simple primitive value
+let age = 35
+
+// A complex value: a Uri
+let website = System.Uri "https://fsharp.org"
+
+// Another complex value: a function
+let add first second = first + second
