4339eae7b9b1ee06e55fe794b3c6d2cea0a465ed
laj <larry.jones@reveal-energy.com>
2022-09-27 18:48:07 -0500

    Listing 36.1
    This commit demonstrates synchronous and asynchronous workflows (with a
    "wait").

---
+++ l36/listing-36.1.fsx
@@ -0,0 +1,13 @@
+﻿// Synchronous flow with "wait"
+printfn $"{System.DateTime.UtcNow:``o``} Loading data synchronously!"
+System.Threading.Thread.Sleep(5000)
+printfn $"{System.DateTime.UtcNow:``o``} Loaded data synchronously!"
+printfn $"{System.DateTime.UtcNow:``o``} My synchronous name is Simon."
+
+// Asynchronous flow with "wait"
+async {
+    printfn $"{System.DateTime.UtcNow:``o``} Loading data asynchronously!"
+    System.Threading.Thread.Sleep(5000)
+    printfn $"{System.DateTime.UtcNow:``o``} Loaded data asynchronously!"
+} |> Async.Start
+printfn $"{System.DateTime.UtcNow:``o``} My asynchronous name is Simon."
