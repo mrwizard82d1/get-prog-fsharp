5a3cc0e1b79e7a09bc62debbc57ea3a18321e2ec
laj <larry.jones@reveal-energy.com>
2022-09-27 19:29:28 -0500

    Listing 36.2
    Previous commits ...
    
    This commit demonstrates returning a value from an `async` block.

---
+++ l36/listing-36.2.fsx
@@ -0,0 +1,21 @@
+﻿// Define an asynchronous block
+// NOTE: the value returned from an `async` block **must** be preceded
+// by the `return` keyword
+let asyncHello : Async<string> = async { return "Hello" }
+
+// Invoking the `Length` property without running raises an exception
+// (Uncomment to see the issue)
+// let length = asyncHello.Length
+
+// Wait for the asynchronous block to finish
+// Think of `Async.RunSynchronously` as _unwrapping_ an asynchronous value
+//
+// Additionally, defining an `async` value **does not** start the block.
+// Instead, one must execute `Async.RunSynchronously` or `Async.Start`.
+//
+// Finally, unlike `Task.Result`, every time one invokes `Run.Asynchronously`,
+// the `async` block runs **anew**.
+let text = asyncHello |> Async.RunSynchronously
+
+// Now I can invoke the `Length` property with no error
+let lengthTwo = text.Length
